2fa8d4d7d8259ccd758dae19cdf8074f9ff70a59
yasuaki640 <yasuaki640@gmail.com>
2019-03-15 11:09:58 +0900

    画像登録昨日の実装

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -19,10 +20,49 @@ class HomeController extends Controller
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        return view('home');
+        $user = User::find(auth()->id());
+
+        return view('home', compact('user'));
+    }
+
+
+    /**
+     * ファイルアップロード処理
+     */
+    public function upload(Request $request)
+    {
+        $this->validate($request, [
+            'file' => [
+                // 必須
+                'required',
+                // アップロードされたファイルであること
+                'file',
+                // 画像ファイルであること
+                'image',
+                // MIMEタイプを指定
+                'mimes:jpeg,png',
+                // 最小縦横120px 最大縦横4000px
+                'dimensions:min_width=120,min_height=120,max_width=4000,max_height=40000',
+            ]
+        ]);
+
+        if ($request->file('file')->isValid([])) {
+            $filename = $request->file->store('public/avatar');
+
+            $user = User::find(auth()->id());
+            $user->avatar_filename = basename($filename);
+            $user->save();
+
+            return redirect('/home')->with('success', '保存しました。');
+        } else {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors(['file' => '画像がアップロードされていないか不正なデータです。']);
+        }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        {!! Form::open(['url' => '/upload', 'method' => 'post', 'files' => true]) !!}
+
+                        {{--成功時のメッセージ--}}
+                        @if (session('success'))
+                            <div class="alert alert-success">{{ session('success') }}</div>
+                        @endif
+                        {{-- エラーメッセージ --}}
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <div class="form-group">
+                            @if ($user->avatar_filename)
+                                <p>
+                                    <img style="max-width: 100%" src="{{ asset('storage/avatar/' . $user->avatar_filename) }}" alt="avatar"/>
+                                </p>
+                            @endif
+                            {!! Form::label('file', '画像アップロード', ['class' => 'control-label']) !!}
+                            {!! Form::file('file') !!}
                         </div>
-                    @endif
 
-                    You are logged in!
+                        <div class="form-group">
+                            {!! Form::submit('アップロード', ['class' => 'btn btn-default']) !!}
+                        </div>
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
